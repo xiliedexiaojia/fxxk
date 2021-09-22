1d0dae61604db11b5e27bf6ec867bc13b192c98a
danteswx <dantes.wx@gmail.com>
2021-09-22 13:26:25 +0800

    更新

--- jd_decompression.js
+++ jd_decompression.js
@@ -1,7 +1,7 @@
 /**
  * 蚊子腿豆子，活动时间:9.21-10.16 10月16号应该可以参与瓜分
  * 第一个号会给作者助力，其他号会给第一个号助力，活动期间貌似只有一次助力机会
- cron  15 4,16 1-16,21-30 9,10 * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_decompression.js
+ cron  5 6,18 1-16,21-30 9,10 * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_decompression.js
  */
 const $ = new Env('热血心跳,狂解压');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -39,8 +39,8 @@ if ($.isNode()) {
         $.oldcookie = cookiesArr[i];
         $.isLogin = true;
         $.nickName = '';
-        await TotalBean();
         $.UserName = decodeURIComponent($.cookie.match(/pt_pin=([^; ]+)(?=;?)/) && $.cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
+        await TotalBean();
         console.log(`\n*****开始【京东账号${$.index}】${$.nickName || $.UserName}*****\n`);
         if (!$.isLogin) {
             $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
@@ -111,10 +111,12 @@ async function main() {
         await $.wait(1000);
     }
     $.score2Flag = true;
-    for (let i = 0; i < score2 && $.score2Flag; i++) {
+    $.score2Time = 0;
+    for (let i = 0; i < score2 && $.score2Flag && $.score2Time< 10; i++) {
         console.log(`进行第${i+1}次扭蛋`);
         await takePostRequest('draw');
         await $.wait(1500);
+        $.score2Time++;
     }
     if($.index === '1'){
         $.shareUuid = $.activityData.actorUuid;
