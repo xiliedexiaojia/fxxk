db98397ee7d9f18a40952f175344fb53493184ac
danteswx <dantes.wx@gmail.com>
2021-10-06 10:57:38 +0800

    更新

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -190,8 +190,8 @@ async function all(cookie, jrBody) {
         JDUserSignPre(stop, 'JDShand', '京东拍拍-二手', '3S28janPLYmtFxypu37AYAGgivfp'), //京东拍拍二手
         JDUserSignPre(stop, 'JDClean', '京东商城-清洁', '2Tjm6ay1ZbZ3v7UbriTj6kHy9dn6'), //京东清洁馆
         JDUserSignPre(stop, 'JDCare', '京东商城-个护', '2tZssTgnQsiUqhmg5ooLSHY9XSeN'), //京东个人护理馆
-        JDUserSignPre(stop, 'JDJiaDian', '京东商城-家电', '3uvPyw1pwHARGgndatCXddLNUxHw'), // 京东小家电
-        // JDUserSignPre(stop, 'JDJewels', '京东商城-珠宝', 'zHUHpTHNTaztSRfNBFNVZscyFZU'), //京东珠宝馆
+        JDUserSignPre(stop, 'JDJiaDian', '京东商城-家电', '3X4HMWmUigG689ZUZAg3Yo8Wtqf5'), // 京东小家电
+        JDUserSignPre(stop, 'JDJewels', '京东商城-趣护', 'yhobhPM9LvDKmJxhRbXnYBko2gY'), //京东珠宝馆
         // JDUserSignPre(stop, 'JDMakeup', '京东商城-美妆', '2smCxzLNuam5L14zNJHYu43ovbAP'), //京东美妆馆
         JDUserSignPre(stop, 'JDVege', '京东商城-菜场', 'Wcu2LVCFMkBP3HraRvb7pgSpt64'), //京东菜场
         // JDUserSignPre(stop, 'JDLive', '京东智能-生活', 'KcfFqWvhb5hHtaQkS4SD1UU6RcQ') //京东智能生活
