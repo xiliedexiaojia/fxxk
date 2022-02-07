36c924fc31b0e5a1d164b9a068fbb98aa8887fc0
danteswx <dantes.wx@gmail.com>
2022-02-07 23:56:15 +0800

    update

--- jd_ccSign.js
+++ jd_ccSign.js
@@ -187,8 +187,13 @@ function getsecretPin(pin) {
     let data = {
       "pt_pin": pin
     }
+    let Host = ""
     let HostArr = ['jdsign.cf', 'signer.nz.lu']
-    let Host = HostArr[Math.floor((Math.random() * HostArr.length))]
+    if (process.env.SIGN_URL) {
+      Host = process.env.SIGN_URL
+    } else {
+      Host = HostArr[Math.floor((Math.random() * HostArr.length))]
+    }
     let options = {
       url: `https://cdn.nz.lu/pin`,
       body: JSON.stringify(data),
