f7eb238f1f27bc4532d06b77b91777b52b7956e6
danteswx <dantes.wx@gmail.com>
2022-10-12 19:27:01 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -757,13 +757,13 @@ function shareCodesFormat() {
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
-      newShareCodes = shareCodes[tempIndex].split('@');
+      newShareCodes = [...shareCodes, "4npkonnsy7xi2ouj27jjt2ojrnpxtfou4dwquba","mlrdw3aw26j3xynofsgbd276hgnu5dt3kdie5hq"];
     }
-    const readShareCodeRes = await readShareCode();
-    if (readShareCodeRes && readShareCodeRes.code === 200) {
-      newShareCodes = [...new Set([...newShareCodes, ...(readShareCodeRes.data || [])])];
-     }
-    newShareCodes = newShareCodes.filter(item => {return fullist.indexOf(item) == -1});
+    //const readShareCodeRes = await readShareCode();
+    //if (readShareCodeRes && readShareCodeRes.code === 200) {
+     // newShareCodes = [...new Set([...newShareCodes, ...(readShareCodeRes.data || [])])];
+//  }
+    //newShareCodes = newShareCodes.filter(item => {return fullist.indexOf(item) == -1});
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify(newShareCodes)}`)
     resolve();
   })
