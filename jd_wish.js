c92ebfe77b3067ed7f48d30601278f3d8c4f0b25
danteswx <dantes.wx@gmail.com>
2022-12-08 14:16:50 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -25,8 +25,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ['1FVRZxKiD','1ElZXwKuP'];
-let appNameArr = ['1111','11.11'];
+let appIdArr = ['1EFVXyw','1ElZXwKuP'];
+let appNameArr = ['金榜盛典','11.11'];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
